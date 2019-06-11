37adf529afc02f4284cda7ed8765d1d30c192b5b
jmartin23 <51715941+jmartin23@users.noreply.github.com>
2019-06-11 13:34:01 -0400

    roject

---
+++ js/project/answers.ts
@@ -0,0 +1,39 @@
+﻿class Answer {
+    private text: string;
+    private next: number;
+    private points: number;
+    private visited: boolean = false;
+    constructor(text: string, next: number, points: number) {
+        this.text = text;
+        this.next = next;
+        this.points = points;
+    }
+
+    visit() {
+        this.visited = true;
+    }
+    getPoints() {
+        return this.points;
+    }
+    getNextDirection() {
+        return this.next;
+    }
+    getAnswer() {
+        return this.text;
+    }
+    hasBeenVisted() {
+        return this.visited;
+    }
+
+}
+/*
+where we put answers
+reponse 9is the answer text itself
+the first number is the direction
+the second number is the points that the answer is worth 
+*/
+var answers: Answer[];
+answers.push(new Answer("Reponse", 1, 10));
+answers.push(new Answer("Reponse2", 0, 0));
+
+arceus.addQuestion(questions[0], answers);
---
+++ js/project/questions.ts
@@ -0,0 +1,14 @@
+﻿class Question {
+    private text: string;
+    constructor(text: string) {
+        this.text = text;
+    }
+    getQuestion() {
+        return this.text;
+    }
+}
+var questions: Question[];
+questions.push(new Question("Question1"));
+questions.push(new Question("Question2"));
+
+arceus.addQuestion(questions[0], answers);
