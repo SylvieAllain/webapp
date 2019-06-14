9c92e823119d2e8a35ce57ba733effc8fa688b75
SylvieAllain <43377680+SylvieAllain@users.noreply.github.com>
2019-06-14 12:49:50 -0400

    commit

--- js/projectSylvie/src/test/choices.spec.ts
+++ js/projectSylvie/src/test/choices.spec.ts
@@ -1,11 +1,11 @@
 ﻿import { Choice } from '../choices';
 
-var choice = new Choice("it's a choice", 0, 1, 10, 4);
+const ANY_CHOICE = new Choice("it's a choice", 0, 1, 10, 4);
 
 test('return the choice text', () => {
-    expect(choice.getChoice()).toBe("it's a choice");
+    expect("it's a choice").toBe(ANY_CHOICE.getChoice());
 });
 
 test("return the next context index", () => {
-
+    expect(1).toBe(ANY_CHOICE.getNextContext());
 })
