877f5540ee817169d839702d12d136e37ac5ba2b
SylvieAllain <43377680+SylvieAllain@users.noreply.github.com>
2019-06-17 09:34:16 -0400

    arceus

--- js/projectJames/arceus.ts
+++ js/projectJames/arceus.ts
@@ -1,47 +1,113 @@
-﻿class Arceus {
+﻿import { Choice } from "./choices";
+import { Context } from "./context";
 
-    private initialContextIndex: number;
-    private context: Context[] = [];
-    private choice: Choice[][] = [];
-    private points: number = 0;
-    private currentContextIndex: number = 0;
-
-    constructor(context: Context[], choice: Choice[][]) {
-        this.context = context;
-        this.choice = choice;
+export class Arceus {
+    static readonly STARTING_POINTS: number = 500;
+    private initialContextIndex: number = 0;
+    private initialChoicesIndex: number = 0;
+    private choices: Choice[][] = [];
+    private contexts: Context[] = [];
+    private points: number = Arceus.STARTING_POINTS;
+    private currentContextIndex: number;
+    private currentChoicesIndex: number;
+    constructor(choices: Choice[][], contexts: Context[]) {
+        this.choices = choices;
+        this.contexts = contexts;
+        this.currentContextIndex = 0;
+        this.currentChoicesIndex = 0;
     }
 
-    giveChoices(index: number) {
-        this.choice[index];
-        this.nextNode;
+    setChoices(userIndex: number): void {
+        if (userIndex == 10) {
+            this.currentContextIndex = this.getPreviousContext();
+            this.currentChoicesIndex = this.getPreviousChoices();
+        }
+        else {
+            var currentChoices: Choice[] = this.choices[this.currentChoicesIndex];
+            var userChoice: Choice = currentChoices[userIndex];
+            this.removePoints(userChoice.getPoints());
+            this.setContext(userChoice);
+            this.currentChoicesIndex = userChoice.getNextChoices();
+        }
+        /*if (this.isThisAnEnding()) {
+            this.reset();
+        }
+        else {
+            this.nextNode();
+        }*/
     }
 
-    getContext(index: number) {
-        return this.context[index];
+    getChoices(): Choice[] {
+        return this.choices[this.currentChoicesIndex];
+    }
+
+    getPreviousContext(): number {
+        var context: Context = this.contexts[this.currentContextIndex];
+        return context.getPrevious();
+    }
+    getPreviousChoices(): number {
+        var choices: Choice[] = this.choices[this.currentChoicesIndex];
+        var choice: Choice = choices[0];
+        return choice.getPrevious();
+    }
+    setContext(userChoice: Choice): void {
+        this.currentContextIndex = userChoice.getNextContext();
     }
 
     getCurrentContextIndex() {
         return this.currentContextIndex;
     }
 
-    getChoices(index: number) {
-        return this.choice[index];
+    getCurrentChoicesIndex() {
+        return this.currentChoicesIndex;
     }
 
-    nextNode(index: number) {
-        this.currentContextIndex = index;
+    displayContent(): void {
+        this.displayContext();
+        this.displayChoices();
+    }
 
-        /*
-        console.log(this.context[this.currentContextIndex].getContext());
-        let answers = this.choice[this.currentContextIndex];
-        answers.forEach(function (choice, i) {
-            console.log((i + 1) + ". " + answer.getChoices());
+    displayContext() {
+        var context: Context = this.contexts[this.currentContextIndex];
+        var space = document.createElement("p");
+        space.setAttribute("id", "mainContext");
+        space.innerHTML = context.getContext();
+        document.body.appendChild(space);
+    }
+
+    displayChoices() {
+        var choices: Choice[] = this.choices[this.currentChoicesIndex];
+        var i: number = 0;
+        var self = this;
+        choices.forEach(function (element, i) {
+            var button = document.createElement("button");
+            button.setAttribute("class", "choiceButton");
+            button.textContent = element.getChoice();
+            button.onclick = function () {
+                self.setChoices(i);
+            }
+            i++;
+            document.body.appendChild(button);
         });
-        */
+        var button = document.createElement("button");
+        button.setAttribute("id", "goBack");
+        button.onclick = function () {
+            self.setChoices(10);
+        }
     }
 
-    addPoints(amount: number) {
-        this.points = this.points + amount;
+    isThisAnEnding(): boolean {
+        return this.contexts[this.currentContextIndex].isEnd();
+    }
+
+    getContext(contextIndex: number): string {
+        var index = contextIndex;
+        console.log(index);
+        return this.contexts[index].getContext();
+    }
+
+    removePoints(amount: number) {
+        this.points = this.points - amount;
         return this.points;
     }
 
@@ -50,12 +116,15 @@
     }
 
     reset() {
-        this.points = 0;
-        this.context[this.initialContextIndex];
+        this.points = Arceus.STARTING_POINTS;
+        this.contexts[this.initialContextIndex];
+        this.choices[this.initialChoicesIndex];
     }
-    
-    start(questionIndex: number) {
-        this.initialContextIndex = questionIndex;
-        this.nextNode(this.initialContextIndex);
+
+    start(contextIndex: number, choicesIndex: number) {
+
+        this.initialContextIndex = contextIndex;
+        this.initialChoicesIndex = choicesIndex;
+        this.displayContent();
     }
 }
