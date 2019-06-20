61f42bc3db96f016fb9335fc8be5ee11ac1905d4
SylvieAllain <43377680+SylvieAllain@users.noreply.github.com>
2019-06-20 09:37:22 -0400

    push

--- js/story4.js
+++ js/story4.js
@@ -27,11 +27,11 @@ choices3.push(new Choice("Ask customer about permissions on documents", 2, 10, 0
 choices3.push(new Choice("Suggest a meeting", 0, 11, 0));
 
 choices4.push(new Choice("Test this expression on customer’s non-prod org", 8, 12, 0));
-choices4.push(new Choice("Ask about displayOnSearchfield to the customer", 5, 13, 0));
+choices4.push(new Choice("Ask about displayOnSearch field to the customer", 5, 13, 0));
 choices4.push(new Choice("Tell the customer that his query seems to be filtering results out", 5, 13, 0));
 
 choices5.push(new Choice("Suggest a meeting", 9, 11, 0));
-choices5.push(new Choice("What would be the full query that does not produce results ?", 9, 14, 2));
+choices5.push(new Choice("What would be the full query that does not produce results ?", 9, 5, 2));
 choices5.push(new Choice("Try rebuilding all your sources", 9, 16, 2));
 
 choices6.push(new Choice("Ok", 0, 11, 0));
@@ -42,27 +42,27 @@ choices7.push(new Choice("Ask for extra logs to buy extra time while you think a
 choices7.push(new Choice("Open a bug report with the R&D department.", 0, 19, 0));
 
 //choices 8-9 are the same just diffrent routes for previous
-choices8.push(new Choice("Test this expression on customer’s non-prod org", 11, 20, 0));
-choices8.push(new Choice("Ask about “displayOnSearch=1” on a slack channel", 0, 21, 0));
-choices8.push(new Choice("Tell customer that displayOnSearch=1 returns no result in nonProd org", 0, 22, 0));
+choices8.push(new Choice("Test this expression on customer’s non-prod org", 11, 12, 0));
+choices8.push(new Choice("Ask about “displayOnSearch=1” on a slack channel", 0, 20, 0));
+choices8.push(new Choice("Tell customer that displayOnSearch=1 returns no result in nonProd org", 0, 21, 0));
 
-choices9.push(new Choice("Ask about “displayOnSearch=1”on a slack channel", 0, 21, 0));
-choices9.push(new Choice("Tell customer that displayOnSearch=1 returns no result in nonProd org", 0, 22, 0));
+choices9.push(new Choice("Ask about “displayOnSearch=1”on a slack channel", 0, 20, 0));
+choices9.push(new Choice("Tell customer that displayOnSearch=1 returns no result in nonProd org", 0, 21, 0));
 
 //choices 10-11 are the same just diffrent routes for previous
-choices10.push(new Choice("Mention to the customer that trial orgs don’t come with free support and close the case.", 0, 23, 0));
-choices10.push(new Choice("Check with the Client Success Manager to see if the customer can get free support", 0, 24, 0));
-choices10.push(new Choice("Trigger a full rebuild on the trial org to see if there is any change on their site.", 0, 25, 0));
-choices10.push(new Choice("Test the full query on the trial org mentioned by the customer.", 0, 26, 0));
+choices10.push(new Choice("Mention to the customer that trial orgs don’t come with free support and close the case.", 0, 22, 0));
+choices10.push(new Choice("Check with the Client Success Manager to see if the customer can get free support", 0, 23, 0));
+choices10.push(new Choice("Trigger a full rebuild on the trial org to see if there is any change on their site.", 0, 24, 0));
+choices10.push(new Choice("Test the full query on the trial org mentioned by the customer.", 0, 25, 0));
 
-choices11.push(new Choice("Mention to the customer that trial orgs don’t come with free support and close the case.", 0, 23, 0));
-choices11.push(new Choice("Check with the Client Success Manager to see if the customer can get free support", 0, 24, 0));
-choices11.push(new Choice("Trigger a full rebuild on the trial org to see if there is any change on their site.", 0, 25, 0));
-choices11.push(new Choice("Test the full query on the trial org mentioned by the customer.", 0, 26, 0));
+choices11.push(new Choice("Mention to the customer that trial orgs don’t come with free support and close the case.", 0, 22, 0));
+choices11.push(new Choice("Check with the Client Success Manager to see if the customer can get free support", 0, 23, 0));
+choices11.push(new Choice("Trigger a full rebuild on the trial org to see if there is any change on their site.", 0, 24, 0));
+choices11.push(new Choice("Test the full query on the trial org mentioned by the customer.", 0, 25, 0));
 
-choices12.push(new Choice("Tell the costumer that there is no match for their displayOnSearch=1 on their customerNonProdOrg1content browser", 0, 27, 0));
-choices12.push(new Choice("Check if there is a match for @displayfOnSearch (without a specific value) on their non-prod org", 0, 28, 0));
-choices12.push(new Choice("See if there is a match in their prod org, just in case", 0, 29, 0));
+choices12.push(new Choice("Tell the customer that there is no match for their displayOnSearch=1 on their customerNonProdOrg1 content browser", 0, 26, 0));
+choices12.push(new Choice("Check if there is a match for @displayfOnSearch (without a specific value) on their non-prod org", 0, 12, 0));
+choices12.push(new Choice("See if there is a match in their prod org, just in case", 0, 28, 0));
 
 choices.push(choices1);
 choices.push(choices2);
@@ -85,8 +85,8 @@ context.push(new Context("I see no results", false, false));
 //context 
 context.push(new Context("Yes, it says No results.", false, false, 0));
 context.push(new Context("Is this a joke to you ?<p><span class='text-action'>[Customer calls their CSM]</span></p><p class='text-important'>You’re in trouble.</p>", false, true, 0));
-context.push(new Context("Customer sends log. You find nothing weird.", false, false, 0));
-context.push(new Context("\“Of course. Please test this for yourself on dev.mysearch.site\”Testing the live website makes you realize that the query looks for documents that match the expression \“displayOnSearch=1\”", false, false, 0));
+context.push(new Context("*Customer sends log.* You find nothing weird.", false, false, 0));
+context.push(new Context("\Of course. Please test this for yourself on \”dev.mysearch.site\”\”. Testing the live website makes you realize that the query looks for documents that match the expression \“displayOnSearch=1\”", false, false, 0));
 
 //context 2
 context.push(new Context("Q: testCQ : Source=mySourceAQ : displayOnSearch=1Org : myTrialOrg2", false, false));
@@ -95,7 +95,7 @@ context.push(new Context("Yes, all my documents have the same title as my keywor
 context.push(new Context("[Customer calls their CSM]You’re in trouble.", false, true));
 
 //context 3
-context.push(new Context("Yes the feilds are searchable.", true, false));
+context.push(new Context("Yes the fields are searchable.", true, false));
 context.push(new Context("All the documents are public.", true, false));
 context.push(new Context("This was solved during the meeting : The customer had set an advanced query expression that was looking for a specific value on a field that was not indexed by Coveo.", false, true));
 //the one above is for every suggest a meeting route 11
@@ -117,9 +117,9 @@ context.push(new Context("Struck by lightning, you die on your way to the coffee
 context.push(new Context("Dissatisfied with the poor quality of your report, your team lead frames you for murder.", false, true));
 
 //context 8
-context.push(new Context("No match for this on customerNonprodOrg1", false, false));
+//context.push(new Context("No match for this on customerNonprodOrg1", false, false));
 context.push(new Context("Your request is completely ignored by everyone, except for a few facepalm emoticons that are added under your request. Customer is closing the case, mentioning that a Coveo solution architect solved the issue during a call. Fail", false, true));
-context.push(new Context("The customer mentions that this is instance is indexing in a personal trial org. Your message got them check for the presence of this field in their content and they solved the issue on their end.Somewhat okay outcome.", false, true));
+context.push(new Context("The customer mentions that is instance is indexing in a personal trial org. Your message got them check for the presence of this field in their content and they solved the issue on their end. Somewhat okay outcome.", false, true));
 
 //ending
 context.push(new Context("The customer contacts their client executive, asking for the support they paid for through their regular prod and non prod orgs. The Client exec sets a meeting and invites you to hop in as well. The issue ends up being solved during the meeting but you sense that everyone is somewhat mad at you for pushing back.", false, true));
@@ -127,16 +127,11 @@ context.push(new Context("The CSM says that this customer did pay for support an
 context.push(new Context("Nothing is solved. The customer notices what you did and mentions that you just broke all sorts of crazy nonsense European laws. You are now wanted by Interpol, the French GIGN, the British Bobbies and by King Arthur’s brave knights. You quit your job and go live in the mountains with a few goats. You are kinda happy but wonder what could have been.", false, true));
 context.push(new Context("After some testing on the org, you notice that even if all indexed content is public, there is no match for the query they are using on their search page. You get back to the customer with a full report, mentioning that their query is looking for a specific value on a field not appearing in the index. The customer sees an error in their cloud v2 extensions and fixes the code, solving the issue in the process. They thank you for your help and tell you to close the case.", false, true));
 context.push(new Context("The customer mentions that this issue is not concerning a source in their non prod org, but that they are using a personal trial org. They insist on a meeting. You go on the meeting and they are passive aggressive. Everyone finds the culprit at the same time but they don’t thank you.", false, true));
-context.push(new Context("No match for this on customerNonprodOrg1.", false, false));
+//context.push(new Context("No match for this on customerNonprodOrg1.", false, false));
 context.push(new Context("There is no indexed content on prod org.", false, false));
 
 //hint
-context.push(new Context("So you now know that the documents are public and the feilds are searchable.", false, false, 7));
-
-//var arceus = new Arceus(context, choices);
-context.push(new Context("So you now know that the documents are public and the feilds are searchable.", false, false, 6));
-context.push(new Context("So you now know that the documents are public and the feilds are searchable.", false, false));
-context.push(new Context("So you now know that the documents are public and the feilds are searchable.", false, false));
+context.push(new Context("So you now know that the documents are public and the fields are searchable.", false, false));
 
 var storyIndex = 4;
 
